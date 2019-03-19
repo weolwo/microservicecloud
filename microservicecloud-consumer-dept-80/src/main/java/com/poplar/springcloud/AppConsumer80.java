8ec973cb16d49ddfa14809175dd536fd36c1acd9
poplar <poplar@qq.com>
2019-03-19 23:27:33 +0800

    Initial commit

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/poplar/springcloud/AppConsumer80.java
@@ -0,0 +1,13 @@
+package com.poplar.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsumer80 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppConsumer80.class,args);
+    }
+}
---
+++ microservicecloud-consumer-dept-80/src/main/java/com/poplar/springcloud/controller/DeptControllerConsumer.java
@@ -0,0 +1,39 @@
+package com.poplar.springcloud.controller;
+
+import com.poplar.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class DeptControllerConsumer {
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    @Autowired
+    private RestTemplate restTemplate;
+    /**
+     * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
+     * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
+     */
+    @RequestMapping(value = "/comsumer/dept/add")
+    public boolean add(Dept dept){
+
+        return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add",dept,Boolean.class);
+    }
+
+    @RequestMapping(value = "/comsumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id){
+
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id,Dept.class);
+    }
+    @RequestMapping(value = "/comsumer/dept/list")
+    public List<Dept> list()
+    {
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
+    }
+
+}
